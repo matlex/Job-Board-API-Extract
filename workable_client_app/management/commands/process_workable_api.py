4eed50d12f8350873a8e34b9c5b078a63f5aa1d1
Matvey Ustinov <matlex@yandex.ru>
2017-04-04 17:49:59 +0600

    Reimplement database models Implement main program data collection and updating logic

--- workable_client_app/management/commands/process_workable_api.py
+++ workable_client_app/management/commands/process_workable_api.py
@@ -1,7 +1,16 @@
+import json
+import os
+
+import datetime
 import requests
 from urllib.parse import urljoin
 
+import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "workable_api.settings")
+django.setup()
+
 from workable_api.secrets.secrets import *
+from workable_client_app.models import *
 
 
 # Workable API Client
@@ -15,7 +24,7 @@ class WorkableAPIClient:
         headers = {'Authorization': self.auth_token, 'Content-Type': 'application/json'}
         url = urljoin(self.root_endpoint, endpoint)
         r = requests.get(url, headers=headers)
-        return r.text
+        return r.json()
 
     def test_api(self):
         """Call to / endpoint"""
@@ -25,7 +34,7 @@ class WorkableAPIClient:
     def get_jobs(self):
         api_endpoint = '{subdomain}/jobs'.format(subdomain=self.subdomain)
         resp = self.get_response(api_endpoint)
-        print(resp)
+        return resp['jobs']
 
     def get_job_data(self, job_shortcode):
         api_endpoint = '{subdomain}/jobs/{shortcode}'.format(subdomain=self.subdomain, shortcode=job_shortcode)
@@ -35,7 +44,7 @@ class WorkableAPIClient:
     def get_job_candidates(self, job_shortcode):
         api_endpoint = '{subdomain}/jobs/{job_shortcode}/candidates'.format(subdomain=self.subdomain, job_shortcode=job_shortcode)
         resp = self.get_response(api_endpoint)
-        print(resp)
+        return resp['candidates']
 
     def get_candidate_info(self, job_shortcode, candidate_id):
         """
@@ -48,17 +57,150 @@ class WorkableAPIClient:
             subdomain=self.subdomain, job_shortcode=job_shortcode, candidate_id=candidate_id
         )
         resp = self.get_response(api_endpoint)
-        print(resp)
+        return resp['candidate']
 
 
 def main():
-    pass
+    workable_client = WorkableAPIClient()
+    wk_jobs = workable_client.get_jobs()
+
+    # Find and iterate over each job
+    for job in wk_jobs:
+        # Add Job data into DB
+        job_obj = Job.objects.get_or_create(
+            title=job['title'],
+            full_title=job['full_title'],
+            department=job['department'],
+            wk_short_code=job['shortcode']
+        )[0]
+
+        # Get all candidates for a job
+        wk_candidates = workable_client.get_job_candidates(job['shortcode'])
+
+        for candidate in wk_candidates:
+            # Get candidate's full data
+            wk_candidate_data = workable_client.get_candidate_info(job['shortcode'], candidate['id'])
+            # print(json.dumps(wk_candidate_data))
+
+            # We will track whether candidate data was updated or not since last check.
+            wk_candidate_updated_at = wk_candidate_data['updated_at']
+            # wk_candidate_updated_at = datetime.datetime.strptime(wk_candidate_updated_at[:-1], "%Y-%m-%dT%H:%M:%S")
+
+            try:
+                candidate_obj = Candidate.objects.get(wk_id=wk_candidate_data['id'])
+                # If candidate with 'id' exists then we need to check was it updated or no
+                # If it was updated then just update all candidate's data
+                if candidate_obj.updated_at.strftime("%Y-%m-%dT%H:%M:%SZ") == wk_candidate_updated_at:
+                    # print('Profile not updated!')
+                    # Interrupt current candidate and move to the next iteration
+                    continue
+                # Change updated_at date and keep going to update other fields
+                else:
+                    candidate_obj.updated_at = wk_candidate_updated_at
+            except Candidate.DoesNotExist:
+                # If candidate with 'id' doesn't exist then we need create a new Candidate record
+                # Prefill some static data
+                candidate_obj = Candidate(
+                    wk_id=wk_candidate_data['id'],
+                    updated_at=wk_candidate_updated_at,
+                    first_name=wk_candidate_data['firstname'],
+                    last_name=wk_candidate_data['lastname']
+                )
+
+            # Then updating candidate_obj instances
+            candidate_obj.job = job_obj
+            candidate_obj.image_url = wk_candidate_data['image_url']
+            candidate_obj.cover_letter = wk_candidate_data['cover_letter']
+            candidate_obj.summary = wk_candidate_data['summary']
+            candidate_obj.resume_url = wk_candidate_data['resume_url']
+            candidate_obj.phone = wk_candidate_data['phone']
+            candidate_obj.email = wk_candidate_data['email']
+
+            # Stage/Status
+            status_obj = Status.objects.get_or_create(title=wk_candidate_data['stage'])[0]
+            candidate_obj.status = status_obj
+
+            candidate_obj.save()
+
+            # Skills
+            try:
+                skills = wk_candidate_data['skills']
+                if skills:
+                    for skill in skills:
+                        skill_obj = Skill.objects.get_or_create(title=skill['name'])[0]
+                        candidate_obj.skills.add(skill_obj)
+            except KeyError:
+                pass
+
+            # Tags
+            try:
+                tags = wk_candidate_data['tags']
+                if tags:
+                    for tag in tags:
+                        tag_obj = Tag.objects.get_or_create(title=tag)[0]
+                        candidate_obj.tags.add(tag_obj)
+            except KeyError:
+                pass
+
+            # Work experience
+            try:
+                work_experience_list = wk_candidate_data['experience_entries']
+                if work_experience_list:
+                    for exp in work_experience_list:
+                        try:
+                            exp_obj = WorkExperience.objects.get(wk_id=exp['id'])
+                        except WorkExperience.DoesNotExist:
+                            exp_obj = WorkExperience(wk_id=exp['id'])
+
+                        exp_obj.title = exp['title']
+                        exp_obj.start_date = exp['start_date']
+                        exp_obj.end_date = exp['end_date']
+                        exp_obj.company = exp['company']
+                        exp_obj.industry = exp['industry']
+                        exp_obj.current = exp['current']
+                        exp_obj.save()
+                        candidate_obj.work_experience.add(exp_obj)
+            except KeyError:
+                pass
+
+            # Social profiles
+            try:
+                social_profiles = wk_candidate_data['social_profiles']
+                if social_profiles:
+                    for sp in social_profiles:
+                        sp_obj = SocialProfile.objects.get_or_create(type=sp['type'], name=sp['name'], url=sp['url'])[0]
+                        candidate_obj.social_profiles.add(sp_obj)
+            except KeyError:
+                pass
+
+            # Education
+            try:
+                wk_education_entries = wk_candidate_data['education_entries']
+                if wk_education_entries:
+                    for edu_entry in wk_education_entries:
+                        try:
+                            education_obj = Education.objects.get(wk_id=edu_entry['id'])
+                        except Education.DoesNotExist:
+                            education_obj = Education(wk_id=edu_entry['id'], school=edu_entry['school'])
+
+                        education_obj.degree = edu_entry['degree']
+                        education_obj.field_of_study = edu_entry['field_of_study']
+                        education_obj.start_date = edu_entry['start_date']
+                        education_obj.end_date = edu_entry['end_date']
+                        education_obj.save()
+                        candidate_obj.education.add(education_obj)
+            except KeyError:
+                pass
+            # Save candidate instance to update it's data
+            candidate_obj.save()
 
 if __name__ == '__main__':
-    workable_client = WorkableAPIClient()
+    main()
 
+    # Test & Debug
+    # workable_client = WorkableAPIClient()
     # workable_client.get_jobs()
     # workable_client.get_job_data('125CFA4F51')
     # workable_client.get_job_candidates('125CFA4F51')
-    workable_client.get_candidate_info('125CFA4F51', '14c06b7')  # Another John Doe
-    workable_client.get_candidate_info('125CFA4F51', '13b8300')  # John Doe
+    # workable_client.get_candidate_info('125CFA4F51', '14c06b7')  # Another John Doe
+    # workable_client.get_candidate_info('125CFA4F51', '13b8300')  # John Doe
--- workable_client_app/models.py
+++ workable_client_app/models.py
@@ -5,6 +5,7 @@ class Job(models.Model):
     title = models.CharField(max_length=255)
     full_title = models.CharField(max_length=255)
     department = models.CharField(max_length=255, null=True)
+    wk_short_code = models.CharField(max_length=50)
 
 
 class Status(models.Model):
@@ -29,24 +30,30 @@ class SocialProfile(models.Model):
 class WorkExperience(models.Model):
     title = models.CharField(max_length=255)
     start_date = models.DateField()
-    end_date = models.DateField()
-    company = models.CharField(max_length=255)
-    industry = models.CharField(max_length=255)
+    end_date = models.DateField(null=True)
+    company = models.CharField(max_length=255, null=True)
+    industry = models.CharField(max_length=255, null=True)
     current = models.BooleanField()
+    wk_id = models.CharField(max_length=15, unique=True)
 
 
 class Education(models.Model):
-    degree = models.CharField(max_length=255)
     school = models.CharField(max_length=255)
-    field_of_study = models.CharField(max_length=255)
-    start_date = models.DateField()
-    end_date = models.DateField()
-    current = models.BooleanField()
+    degree = models.CharField(max_length=255, null=True)
+    field_of_study = models.CharField(max_length=255, null=True)
+    start_date = models.DateField(null=True)
+    end_date = models.DateField(null=True)
+    wk_id = models.CharField(max_length=15, unique=True)
 
 
 class Candidate(models.Model):
-    # One line fields
+    # FKeys
     job = models.ForeignKey(Job, related_name='candidates')
+    status = models.ForeignKey(Status, related_name='candidates')
+
+    # One line fields
+    wk_id = models.CharField(max_length=50, unique=True)
+    updated_at = models.DateTimeField()
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     image_url = models.URLField(max_length=500, null=True)
@@ -54,8 +61,7 @@ class Candidate(models.Model):
     summary = models.CharField(max_length=1000, null=True)
     resume_url = models.URLField(max_length=500, null=True)
     phone = models.CharField(max_length=30, null=True)
-    email = models.EmailField(max_length=255)
-    status = models.ForeignKey(Status, related_name='candidates')
+    email = models.EmailField(max_length=255, blank=True)
 
     # ManyToMany fields
     skills = models.ManyToManyField(Skill)
@@ -63,3 +69,6 @@ class Candidate(models.Model):
     work_experience = models.ManyToManyField(WorkExperience)
     social_profiles = models.ManyToManyField(SocialProfile)
     education = models.ManyToManyField(Education)
+
+    def __str__(self):
+        return "{} {} - {}".format(self.first_name, self.last_name, self.job.title)
